68037f6c67330887a2414f1f3e315cad65c81064
Madhavi-Kadu <madhavikadu6@gmail.com>
2025-10-25 18:54:55 +0530

    javaScriptUploads

---
+++ javaScript/first.js
@@ -0,0 +1,10 @@
+console.log("Hello world");
+console.log("i am LM");
+age = 21;
+console.log(age);
+let fullName =    "hello";
+console.log(fullName);
+
+
+
+console.log()
---
+++ javaScript/practiceQ2.js
@@ -0,0 +1,9 @@
+let gameNum = 25;
+
+let userNum = prompt("Guess the number : ");
+
+while(userNum !== gameNum){
+    console.log("you enter a wrong number enter again");
+
+}
+console.log("congratualation, you entered the right number");
